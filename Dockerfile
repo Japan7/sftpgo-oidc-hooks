1d5d65902ca287e5c8b2690e5dbe9e7d4ff0d388
NextFire <git@yuru.moe>
2024-01-06 01:37:50 +0100

    init

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM lukechannings/deno:latest
+
+WORKDIR /app
+
+COPY main.ts .
+RUN deno cache main.ts
+
+CMD ["run", "--allow-net", "main.ts"]
+EXPOSE 8000
---
+++ main.ts
@@ -0,0 +1,30 @@
+import { logger } from "https://deno.land/x/hono@v3.12.0/middleware.ts";
+import { Hono } from "https://deno.land/x/hono@v3.12.0/mod.ts";
+
+const app = new Hono();
+
+app.use("*", logger());
+
+app.post("/prelogin", async (c) => {
+  const payload = await c.req.json();
+
+  const username = payload.username;
+  const groups = payload.oidc_custom_fields.groups.map((g: string) => ({
+    name: g.slice(1).replace("discord-", ""),
+    type: 2,
+  }));
+
+  const user = {
+    status: 1,
+    username,
+    groups,
+    permissions: {
+      "/": ["list"],
+    },
+  };
+  console.log(user);
+
+  return c.json(user);
+});
+
+Deno.serve(app.fetch);
